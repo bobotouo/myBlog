e362df42aee1b763db3eec63b693b3a38b7cef62
xuyiheng <676610503@qq.com>
2023-07-04 09:34:09 +0800

    Site updated: 2023-07-04 09:34:09

--- config/js/global.js
+++ config/js/global.js
@@ -1,12 +1,12 @@
 
 
-
 var flag = true;
 function once() {
     if (flag) {
         console.log("我被调用, 只调用一次");
         flag = false;
         window.aplayers[0].play()
+        // window.aplayers[0].lrc.hide()
     } else {
         return;
     }
@@ -29,3 +29,31 @@ document.addEventListener('touchend',function(){
         once()
     }
 })
+
+if (document.querySelector('#bber-talk')) {
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical', 
+      loop: true,
+      autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true
+    },
+    });
+  }
+
+
+
+window.onpageshow = function(options){
+    console.log('加载')
+}
+
+
+function jumpToComment(text){
+    ///跳转到评论, 并输入
+
+    console.log('跳转评论')
+    let textera = document.querySelector('#wl-edit')
+    // 在吧内容给填上
+    textera.value = ' >'+text +'\n\n'
+    textera.focus()
+}
